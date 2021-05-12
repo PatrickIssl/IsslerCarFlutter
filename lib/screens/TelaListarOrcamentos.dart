0245d03b586ed40b0f796d38d6b042bbbb9057ab
Patrick Issler <patrickissler1234@gmail.com>
2021-05-12 17:29:10 -0300

    Tela Listar Orçamentos completa

--- lib/screens/TelaListarOrcamentos.dart
+++ lib/screens/TelaListarOrcamentos.dart
@@ -12,8 +12,19 @@ class _TelaListarOrcamentosState extends State<TelaListarOrcamentos> {
   @override
   Widget build(BuildContext context) {
    return Scaffold(
+
+     appBar: AppBar(
+       title:Row(
+         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+         children: [
+           Text("Selecione o carro desejado"),
+         ],
+       ),
+
+     ),
+
      body: StreamBuilder(
-       stream: Firestore.instance.collection('orcamentos').snapshots(),
+       stream: Firestore.instance.collection('orcamentos').where('status', isEqualTo: "novo").snapshots(),
        builder: (BuildContext context, AsyncSnapshot snapshot){
 
          if(snapshot.hasError){
@@ -30,8 +41,37 @@ class _TelaListarOrcamentosState extends State<TelaListarOrcamentos> {
                  children: snapshot.data.documents.map<Widget>((DocumentSnapshot doc){
                    return ListTile(
                      leading:  Icon(Icons.book, size: 52,),
-                     title: Text("Nome do cliente: ${doc.data['nome_do_cliente']} | Número : ${doc.data['numero_do_cliente']}"),
+                     title: Text("Cliente: ${doc.data['nome_do_cliente']} | Número : ${doc.data['numero_do_cliente']}"),
                      subtitle: Text("Carro: ${doc.data['modelo']} | placa : ${doc.data['placa']} | Cor: ${doc.data['cor_do_veiculo']}"),
+                     onTap: (){
+                       showDialog(context: context, builder: (context){
+                         return AlertDialog(
+                           title: Text("Dados Selecionados para edição :"),
+                           titlePadding: EdgeInsets.all(25),
+                           titleTextStyle: TextStyle(
+                               fontSize: 20,
+                               color: Colors.orange
+                           ),
+                           content: Text("Cliente: ${doc.data['nome_do_cliente']} | Carro: ${doc.data['modelo']} | Placa ${doc.data['placa']}"),
+                           actions: <Widget>[
+                             FlatButton(
+                                 onPressed: (){
+                                   print("Selecionado sim");
+                                   Navigator.pop(context);
+                                 },
+                                 child: Text("Correto")
+                             ),
+                             FlatButton(
+                                 onPressed: (){
+                                   Navigator.pop(context);
+                                 },
+                                 child: Text("Errado")
+                             ),
+                           ],
+                           contentPadding: EdgeInsets.all(20),
+                         );
+                       });
+                     },
                    );
                  }).toList(),
                ),
